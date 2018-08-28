31d76b486013f4a4edfc109009590a225e8a8b0a
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-28 04:09:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,11 @@ def countdown(number)
   until number == 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    countdown_with_sleep
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  sleep(1)
 end
